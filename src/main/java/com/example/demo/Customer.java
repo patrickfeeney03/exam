f9b22b8f0dd7d766468f5eae148bbe4d911c9786
Patrick <G00409592@atu.ie>
2024-10-21 17:07:32 +0100

    had to create a new proj

---
+++ src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    @NotEmpty @Length(min = 2)
+    private String firstName;
+    @NotEmpty @Length(min = 2)
+    private String lastName;
+    @Email
+    private String email;
+    @Digits(integer = 10, fraction =  0)
+    private String phoneNumber;
+    //    @Pattern(regexp = )
+    private String dateOfBirth;
+    @Pattern(regexp = "^[A-Z][0-9]{6}") // any letter followed by 6 ints
+    private String eircode;
+
+
+}
+
